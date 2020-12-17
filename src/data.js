6445470b2b6f0aadb46bf0d3fe286361935f134e
Jefferson A. Silva <jeff2248@hotmail.com>
2020-12-17 14:15:22 -0300

    projeto finalizado

---
+++ src/data.js
@@ -0,0 +1,40 @@
+const reviews = [
+  {
+    id: 1,
+    name: "Carla Oliveria",
+    job: "web developer",
+    image:
+      "https://res.cloudinary.com/diqqf3eq2/image/upload/v1586883334/person-1_rfzshl.jpg",
+    text:
+      "Lorem ipsum fringilla facilisis cursus nullam placerat iaculis enim feugiat volutpat inceptos, nisl turpis per fames metus nam fames iaculis elit etiam condimentum diam, praesent risus ut suscipit morbi litora ligula vivamus tincidunt ipsum. volutpat vulputate justo taciti integer sagittis dictumst",
+  },
+  {
+    id: 2,
+    name: "Pamela Santos",
+    job: "web designer",
+    image:
+      "https://res.cloudinary.com/diqqf3eq2/image/upload/v1586883409/person-2_np9x5l.jpg",
+    text:
+      "Lorem ipsum fringilla facilisis cursus nullam placerat iaculis enim feugiat volutpat inceptos, nisl turpis per fames metus nam fames iaculis elit etiam condimentum diam, praesent risus ut suscipit morbi litora ligula vivamus tincidunt ipsum. volutpat vulputate justo taciti integer sagittis dictumst.",
+  },
+  {
+    id: 3,
+    name: "Paulo Henrique",
+    job: "intern",
+    image:
+      "https://res.cloudinary.com/diqqf3eq2/image/upload/v1586883417/person-3_ipa0mj.jpg",
+    text:
+      "Lorem ipsum fringilla facilisis cursus nullam placerat iaculis enim feugiat volutpat inceptos, nisl turpis per fames metus nam fames iaculis elit etiam condimentum diam, praesent risus ut suscipit morbi litora ligula vivamus tincidunt ipsum. volutpat vulputate justo taciti integer sagittis dictumst.",
+  },
+  {
+    id: 4,
+    name: "Anderson Gomes",
+    job: "the boss",
+    image:
+      "https://res.cloudinary.com/diqqf3eq2/image/upload/v1586883423/person-4_t9nxjt.jpg",
+    text:
+      "Lorem ipsum fringilla facilisis cursus nullam placerat iaculis enim feugiat volutpat inceptos, nisl turpis per fames metus nam fames iaculis elit etiam condimentum diam, praesent risus ut suscipit morbi litora ligula vivamus tincidunt ipsum. volutpat vulputate justo taciti integer sagittis dictumst",
+  },
+];
+
+export default reviews;
